9e4d5ff404706b0092e546959c08356649defd46
Bulat Shakirzyanov <mallluhuct@gmail.com>
2012-09-09 20:43:26 -0700

    remove mixed in method from type metadata

--- lib/frank/type_metadata.rb
+++ lib/frank/type_metadata.rb
@@ -1,12 +1,5 @@
 module Frank
   class TypeMetadata
     include Metadata
-
-    def children(object, &block)
-      object.each_with_index(&block)
-    rescue NoMethodError
-      raise "metadata for #{@type.inspect} contains children metadata, however " +
-            "#{object.inspect}.each_with_index is not defined"
-    end
   end
 end
