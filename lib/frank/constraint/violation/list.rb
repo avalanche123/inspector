6892f94276e2c663d8f0c85d222a50edbf9a4e52
Bulat Shakirzyanov <mallluhuct@gmail.com>
2012-09-23 00:41:58 -0700

    move methods and fix #length

--- lib/frank/constraint/violation/list.rb
+++ lib/frank/constraint/violation/list.rb
@@ -7,6 +7,52 @@ module Frank
           @children = children
         end
 
+        def empty?
+          @violations.empty? && @children.empty?
+        end
+
+        def length
+          length  = @violations.length
+          length += @children.values.map(&:length).reduce(:+) unless @children.empty?
+
+          length
+        end
+
+        def <<(violation)
+          unless violation.kind_of?(Frank::Constraint::Violation)
+            raise "#{violation.inspect} is not a Frank::Constraint::Violation"
+          end
+
+          @violations << violation
+        end
+        alias :push :<<
+
+        def []=(property_path, violations_list)
+          unless violations_list.kind_of?(Frank::Constraint::Violation::List)
+            raise "#{violations_list.inspect} is not a Frank::Constraint::Violation::List"
+          end
+
+          @children[property_path.to_str] = violations_list
+        end
+
+        def [](property_path)
+          child_path, _, property_path = property_path.to_str.split(/(\.|\[|\])/, 2)
+
+          not_found       = "cannot locate violations for #{property_path}"
+          violations_list = @children.fetch(child_path) { raise not_found }
+
+          if property_path
+            violations_list[property_path]
+          else
+            violations_list
+          end
+        end
+
+        def each(*args, &block)
+          @violations.each(*args, &block)
+          @children.values.each { |list| list.each(*args, &block) }
+        end
+
         def to_s
           string = ""
 
@@ -25,57 +71,10 @@ module Frank
         end
 
         def inspect
-          attributes = {
-            :at            => @property_path.inspect,
-            :value         => @value.inspect,
+          "#<violations %{violations} children=%{children}>" % {
             :violations    => @violations.inspect,
             :children      => @children.inspect
-          }.inject("") {|string, pair| "#{string} #{pair.first}=#{pair.last}"}
-
-          "#<violations#{attributes}>"
-        end
-
-        def empty?
-          @violations.empty? && @children.empty?
-        end
-
-        def length
-          @violations.length + @children.values.map(&:length).reduce(:+)
-        end
-
-        def <<(violation)
-          unless violation.kind_of?(Frank::Constraint::Violation)
-            raise "#{violation.inspect} is not a Frank::Constraint::Violation"
-          end
-
-          @violations << violation
-        end
-        alias :push :<<
-
-        def []=(property_path, violations_list)
-          unless violations_list.kind_of?(Frank::Constraint::Violation::List)
-            raise "#{violations_list.inspect} is not a Frank::Constraint::Violation::List"
-          end
-
-          @children[property_path] = violations_list
-        end
-
-        def [](property_path)
-          child_path, _, property_path = property_path.split(/(\.|\[|\])/, 2)
-
-          not_found       = "cannot locate violations for #{property_path}"
-          violations_list = @children.fetch(child_path) { raise not_found }
-
-          if property_path
-            violations_list[property_path]
-          else
-            violations_list
-          end
-        end
-
-        def each(*args, &block)
-          @violations.each(*args, &block)
-          @children.values.each { |list| list.each(*args, &block) }
+          }
         end
       end
     end
